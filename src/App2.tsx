5056fee46a644f07ce7dc9916e67417df7f6f61c
Guruprasad3n <mr.guruprasadb@gmail.com>
2024-01-24 19:18:01 +0530

    Added Router for Analytics

---
+++ src/App2.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import {
+  GetScenesByLectureId,
+  GetSegmentsBySceneId,
+  GetUsersPerScene,
+} from "./graphql/queries"; // Import your queries
+import client from "./graphql/client";
+// import Presentation from "./components/persentation/Presentation";
+import "./components/presentation/Presentation.css";
+import Background from "./utils/Background";
+import FullScreenButton from "./components/fullScreen/FullScreenButton";
+import Presentation_2 from "./components/presentation/Presentation_2";
+import Demo from "./components/Demo";
+import { Link } from "react-router-dom";
+
+interface Lecture {
+  id: number;
+  title: string;
+  type: string;
+}
+// Preleaf Image = "https://static.prepleaf.com/brand/prepleaf/prepleaf-dark.svg"
+
+//https://masai-website-images.s3.ap-south-1.amazonaws.com/logo.png
+function App2() {
+  const [logoUrl, setLogoUrl] = useState<string>(
+    "https://static.prepleaf.com/brand/prepleaf/prepleaf-dark.svg"
+  );
+  const [scenes, setScenes] = useState<any[]>([]);
+  const [segments, setSegments] = useState<any[]>([]);
+
+  const startLecture = async () => {
+    const scenesData = await fetchScenes(73739);
+    setScenes(scenesData);
+    console.log("scenes", scenesData);
+    const segmentsData = await fetchSegments(scenesData[0]?.id);
+    setSegments(segmentsData);
+    // await fetchUsersPerScene();
+  };
+  const fetchScenes = async (id: number) => {
+    try {
+      const { data } = await client.query({
+        query: GetScenesByLectureId,
+        variables: { lectureId: id },
+      });
+      // console.log("scenes", data);
+      return data.getScenesByLectureId;
+    } catch (error: any) {
+      console.error("Error fetching scenes:", error);
+      return null;
+    }
+  };
+
+  const fetchSegments = async (id: number) => {
+    try {
+      const { data } = await client.query({
+        query: GetSegmentsBySceneId,
+        variables: { sceneId: id },
+      });
+      console.log("segments", data);
+      return data.getSegmentsBySceneId;
+    } catch (error: any) {
+      console.error("Error fetching segments:", error);
+      return null;
+    }
+  };
+
+  const nextSceneSegments = async (sceneId: any) => {
+    const segmentsData = await fetchSegments(sceneId);
+    setSegments(segmentsData);
+  };
+
+  return (
+    <Background>
+      <div className="App">
+        <div className="Lecture">
+          {segments.length === 0 ? (
+            <button
+              className="hover:bg-ms-red-600"
+              onClick={() => startLecture()}
+            >
+              Start Lecture
+            </button>
+          ) : null}
+        </div>
+        {segments?.length > 0 && (
+          <Presentation_2
+            segments={segments}
+            scenes={scenes}
+            nextSceneSegments={nextSceneSegments}
+          />
+        )}
+        {/* <div className="full-screen-button">
+          <FullScreenButton />
+        </div> */}
+         <Link to={"/anylit"} >Demo</Link>
+      </div>
+    </Background>
+  );
+}
+
+export default App2;
--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -3,39 +3,54 @@ import client from "../graphql/client";
 import { GetUsersPerScene } from "../graphql/queries"; // Import your queries
 
 interface userScenes {
-    scene_id: number;
-    order: number;
-    users: number;
+  scene_id: number;
+  order: number;
+  users: number;
 }
 
 const Demo = () => {
-    const [ data, setData ] = useState<userScenes[]>();
+  const [data, setData] = useState<userScenes[]>();
 
-    const fetchUsersPerScene = async () => {
-        try {
-          const { data, errors } = await client.query({
-            query: GetUsersPerScene,
-          });
-      
-          if (errors) {
-            console.error("GraphQL Errors:", errors);
-          }
-      
-          if (data) {
-            setData(data.getUsersPerScene);
-            console.log("Users per scene:", data.getUsersPerScene);
-          }
-        } catch (error: any) {
-          console.error("Error fetching users per scene:", error);
-        }
-      };
+  const fetchUsersPerScene = async () => {
+    try {
+      const { data, errors } = await client.query({
+        query: GetUsersPerScene,
+      });
 
-      useEffect(() => {
-        fetchUsersPerScene().then(res => console.log(data));
-      }, [data])
+      if (errors) {
+        console.error("GraphQL Errors:", errors);
+      }
 
+      if (data) {
+        console.log("Guru Data", data);
+        setData(data.getUsersPerScene);
+        console.log("Users per scene:", data.getUsersPerScene);
+      }
+    } catch (error: any) {
+      console.error("Error fetching users per scene:", error);
+    }
+  };
 
-  return <div>Demo</div>;
+  useEffect(() => {
+    fetchUsersPerScene().then((res) => console.log(data));
+  }, [data]);
+
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident iusto
+      debitis alias, possimus soluta autem eos repellat quo error eligendi
+      necessitatibus ut numquam doloremque assumenda quaerat! Aspernatur fugiat
+      deleniti neque suscipit perferendis dolore tempora adipisci, unde
+      doloremque sit, voluptate assumenda dolores libero quae exercitationem,
+      ipsum sunt quam voluptates maxime maiores magnam voluptatibus sapiente!
+      Ullam, nobis possimus magni et iure saepe repudiandae cum non excepturi
+      tempora error voluptas eligendi. Error ducimus, similique, delectus
+      dignissimos suscipit mollitia itaque accusantium porro repellat architecto
+      pariatur, voluptas assumenda. Excepturi at repellat dignissimos! Neque
+      inventore, harum perspiciatis dolore, quaerat expedita perferendis quae,
+      commodi quia natus id.
+    </div>
+  );
 };
 
 export default Demo;
