2a083a92c3077b6c55454f6d71459b3670f915ba
Guruprasad3n <mr.guruprasadb@gmail.com>
2024-01-25 21:01:49 +0530

    .

--- src/components/presentation/Presentation_2.tsx
+++ src/components/presentation/Presentation_2.tsx
@@ -114,12 +114,12 @@ const Presentation_2: React.FC<PresentationProps> = ({
     if (current.type == "assessment") {
       setIsAssessment(true);
       await createUserSegment({
-        variables: { userId: 5, segmentId: current.id, data: current.data },
+        variables: { userId: 3, segmentId: current.id, data: current.data },
       });
     } else if (current.type == "feedback" || current.type == "revisit") {
       setIsAssessment(false);
       await createUserSegment({
-        variables: { userId: 5, segmentId: current.id, data: current.data },
+        variables: { userId: 3, segmentId: current.id, data: current.data },
       });
       const nextStep = JSON.parse(
         localStorage.getItem("next_scene")!
@@ -129,20 +129,20 @@ const Presentation_2: React.FC<PresentationProps> = ({
       if (nextStep.success) {
         // Call mutation to create user scene
         await createUserScene({
-          variables: { userId: 5, sceneId: currentScene?.id },
+          variables: { userId: 3, sceneId: currentScene?.id },
         });
         nextSceneSegments(currentScene?.successSceneId || 0);
       } else {
         // Call mutation to create user scene
         await createUserScene({
-          variables: { userId: 5, sceneId: currentScene?.id },
+          variables: { userId: 3, sceneId: currentScene?.id },
         });
         nextSceneSegments(currentScene?.failureSceneId || 0);
       }
     } else {
       // Call mutation to create user segment
       await createUserSegment({
-        variables: { userId: 5, segmentId: current.id, data: current.data },
+        variables: { userId: 3, segmentId: current.id, data: current.data },
       });
       setCurrentSegment(getNextSegment() || ({} as Segment));
     }
